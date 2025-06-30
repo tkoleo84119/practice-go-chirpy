2ae7ec656458bb30efe322e844165ced1d0833ef
Kerwin <tkoleo84119@gmail.com>
2025-06-30 15:25:26 +0800

    add index html

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	ser := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(ser.ListenAndServe())
 }
